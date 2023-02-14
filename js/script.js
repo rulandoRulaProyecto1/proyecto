dbe29470d23a31fe70cd91579cfb440e6011baad
alberto <acarreteron04@educarex.es>
2023-02-14 17:24:10 +0100

    trabajo_asignado_script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
-/*...*/
-function colocarBolas() {}
-/*...*/
-function rellenar(numMax) {}
-/*...*/
-function rellenarCarton() {}
-/*...*/
-function jugar() {}
+let bolasJugadas = [];
+let carton = [];
+let tamañoCarton = 16;
+let tamañoBombo = 20;
+
+function jugar() {
+    let numBola = cogerUnaBola();
+    eliminarBolaSacada(numBola)
+}
+
+/*alberto*/
+function cogerUnaBola(){
+
+}
+
+function obtenerAleatorio() {
+
+}
+
+/*fran*/
+function eliminarBolaSacada(numBola) {
+
+}
+
+function tacharBolaSacada(numBola) {
+
+}
+
+/*jose*/
+function comprobarAciertos(numeroBola){
+
+}
+
+/*dani*/
+function colocarBolas() {
+
+}
+
+function crearCarton() {
+
+}
+
+function borrarTodo() {
+
+}
