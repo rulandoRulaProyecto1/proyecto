3051ef99e2ebf223914433c28e6f593969163394
fragomdia <fmgomezd02@educarex.es>
2023-02-14 08:39:29 +0100

    jugar_alpha

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-/*...*/
-function colocarBolas() {}
-/*...*/
-function rellenar(numMax) {}
-/*...*/
-function rellenarCarton() {}
-/*...*/
+/*El botón Jugar cogerá una bola del bombo y ésta bola se eliminará del mismo y
+aparecerá donde pone “Bola de Bingo”, y si coincide dicha bola con alguna del cartón,
+ésta deberá tacharse.
+Cuando todas las líneas del cartón estén tachadas se mostrará un mensaje indicando
+“Línea” y cuando todas las bolas del cartón estén tachadas, el mensaje mostrará
+“Bingo” y acabará el juego.*/
 function jugar() {}
