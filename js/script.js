d1192801e95e380dbfb3f414d6576014ebcd87f7
fragomdia <fmgomezd02@educarex.es>
2023-02-14 08:33:33 +0100

    colocarBolas_alpha

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-/*...*/
+/*Al pulsar el botón “Colocar Bolas” cogerá del bombo todas las bolas y las colocará en la
+parte superior. Hay que tener en cuenta que el tamaño del bombo será variable, por lo
+que para las pruebas pondremos por ejemplo 20 bolas y posteriormente podremos
+modificar este valor a 100. Al pulsar el botón “Colocar Bolas” hará que se muestre el
+botón Jugar que estaba oculto al principio.*/
 function colocarBolas() {}
-/*...*/
-function rellenar(numMax) {}
-/*...*/
-function rellenarCarton() {}
-/*...*/
-function jugar() {}
