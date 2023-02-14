2b2b67544a9247a3fd2c696174177c2b358d8d21
fragomdia <fmgomezd02@educarex.es>
2023-02-14 08:37:47 +0100

    rellenarCarton_alpha

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-/*...*/
-function colocarBolas() {}
-/*...*/
-function rellenar(numMax) {}
-/*...*/
+/*Una vez colocadas las bolas en el bombo, se creará un cartón cogiendo aleatoriamente
+bolas del bombo hasta un tamaño indicado, podemos empezar con un tamaño de
+cartón de 16 y éste se podrá ampliar en un futuro. Hay que tener en cuenta que las
+bolas del cartón no se pueden repetir.*/
 function rellenarCarton() {}
-/*...*/
-function jugar() {}
