7a1f5f0870748d003b1d4936af7509ab3d0424ff
joseakasty01 <jacastilla02@gmail.com>
2023-02-15 18:11:06 +0100

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ parte superior. Hay que tener en cuenta que el tamaño del bombo será variable,
 que para las pruebas pondremos por ejemplo 20 bolas y posteriormente podremos
 modificar este valor a 100. Al pulsar el botón “Colocar Bolas” hará que se muestre el
 botón Jugar que estaba oculto al principio.*/
-function colocarBolas() {}
+function colocarBolas() {
+    borrarTodo() //Primero eliminamos todas las bolas si las hay
+    for (let i = 1; i <= tamañoBombo; i++) {
+        $("#bolasDisponibles").append("<div class='bola'><p>" + i + "</p></div>")
+    }
+    $("#btnJugar").css("visibility", "visible")
+    crearCarton();
+}
